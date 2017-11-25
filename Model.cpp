0cebe0e076f0ecfddaca35f80f87714011e4f026
Dado1513 <dave.caputo93@gmail.com>
2017-11-25 23:24:22 +0100

    add front and back pacman

--- Model.cpp
+++ Model.cpp
@@ -40,8 +40,10 @@ bool MyModel::InitGL(void)
 
 void MyModel::ReSizeGLScene(int width, int height)
 {
-	if (height==0) height=1;					// Prevent A Divide By Zero By
-	if (width==0) width=1;					// Prevent A Divide By Zero By
+	if (height==0) 
+		height=1;					// Prevent A Divide By Zero By
+	if (width==0) 
+		width=1;					// Prevent A Divide By Zero By
 
 	glViewport(0,0,width,height);						// Reset The Current Viewport
 
@@ -49,23 +51,26 @@ void MyModel::ReSizeGLScene(int width, int height)
 	glLoadIdentity();									// Reset The Projection Matrix
 
 	// orthographic projection
-  // background in [-1, 1] on both x and y
-  // fill th background according to the window aspect ratio
-  // void WINAPI glOrtho( GLdouble left,   GLdouble right,
-  //                      GLdouble bottom, GLdouble top,
-  //                      GLdouble zNear,  GLdouble zFar );
-  if( width >= height ) {
-    this->plx = 1.0;
-    this->ply = 1;
-  } else {
-    this->plx = (double) width /(double) height;
-    this->ply = 1.0;
-  }
-  glOrtho(-this->plx, this->plx, -this->ply, this->ply, 1, 5.1);
+	// background in [-1, 1] on both x and y
+	// fill th background according to the window aspect ratio
+	// void WINAPI glOrtho( GLdouble left,   GLdouble right,
+	//                      GLdouble bottom, GLdouble top,
+	//                      GLdouble zNear,  GLdouble zFar );
+	if( width >= height ) 
+	{
+		this->plx = 1.0;
+		this->ply = 1.0;
+	} 
+	else 
+	{
+		this->plx = (double) width /(double) height;
+		this->ply = 1.0;
+	}
+	glOrtho(-this->plx, this->plx, -this->ply, this->ply, 1, 5.1);
   
-  //  saving the window width and height in pixels
-  this->Wheight = height;
-  this->Wwidth = width;
+	//  saving the window width and height in pixels
+	this->Wheight = height;
+	this->Wwidth = width;
 
 	glMatrixMode(GL_MODELVIEW);							// Select The Modelview Matrix
 	glLoadIdentity();									// Reset The Modelview Matrix
@@ -77,25 +82,23 @@ void MyModel::ReSizeGLScene(int width, int height)
 bool MyModel::LoadGLTextures(void)
 {
 	//load an image file directly as a new OpenGL texture */
-	texture[0] = SOIL_load_OGL_texture
-		( "Data/media/bmps/baseWall.png",
-		SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y );
-	if(texture[0] == 0) return false;
+	texture[0] = SOIL_load_OGL_texture( "Data/media/bmps/baseWall.png",SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y );
+	if(texture[0] == 0) 
+		return false;
 
-	texture[1] = SOIL_load_OGL_texture
-		("Data/media/bmps/mario_test_4.png",
-		SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
-	if (texture[1] == 0) return false;
+	texture[1] = SOIL_load_OGL_texture("Data/media/bmps/mario_test_4.png",SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
+	if (texture[1] == 0) 
+		return false;
 
 	
-  //  Load 11 pacman textures
-  char ll[200];
-  for(int i=1; i < 11; i++) {
-    sprintf(ll,"Data/PacmanSprite/pacman%01d.png",i+1);
-    this->texture[i+1] = SOIL_load_OGL_texture (
-      ll,	SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID,	SOIL_FLAG_INVERT_Y);
-	  if(texture[i+1] == 0) return false;
-  }
+	//  Load 19 pacman textures (front and back)
+	char ll[200];
+	for(int i=1; i < 19; i++) {
+	sprintf(ll,"Data/PacmanSprite/pacman%01d.png",i+1);
+	this->texture[i+1] = SOIL_load_OGL_texture (ll,	SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID,	SOIL_FLAG_INVERT_Y);
+		if(texture[i+1] == 0)
+			return false;
+	}
 
 	// Typical Texture Generation Using Data From The Bitmap
 	glBindTexture(GL_TEXTURE_2D, texture[0]);
@@ -109,30 +112,29 @@ bool MyModel::LoadGLTextures(void)
 
 bool MyModel::DrawGLScene(void)
 {
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	// Clear The Screen And The Depth Buffer
-  glMatrixMode(GL_MODELVIEW);				// Select The Modelview Matrix
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	// Clear The Screen And The Depth Buffer
+	glMatrixMode(GL_MODELVIEW);				// Select The Modelview Matrix
 	glLoadIdentity();									// Reset The View
 
-  //  TIMING - start
-  clock_t t = clock();
-  // elapsed time in seconds from the last draw
-  double elapsed = double (t - Tstamp) /  (double) CLOCKS_PER_SEC;
-  // elapsed time in milliseconds from the last draw
-  int ms_elapsed = (int) (t - Tstamp);
-  // elapsed time in seconds from the beginning of the program
-  this->Full_elapsed = double (t - Tstart) /  (double) CLOCKS_PER_SEC;
-  this->frameTime += double (t - Tstamp) /  (double) CLOCKS_PER_SEC;
+	//  TIMING - start
+	clock_t t = clock();
+	// elapsed time in seconds from the last draw
+	double elapsed = double (t - Tstamp) /  (double) CLOCKS_PER_SEC;
+	// elapsed time in milliseconds from the last draw
+	int ms_elapsed = (int) (t - Tstamp);
+	// elapsed time in seconds from the beginning of the program
+	this->Full_elapsed = double (t - Tstart) /  (double) CLOCKS_PER_SEC;
+	this->frameTime += double (t - Tstamp) /  (double) CLOCKS_PER_SEC;
 
-  this->Tstamp = t;
-  //  TIMING - end
+	this->Tstamp = t;
+	//  TIMING - end
 
 	glDisable(GL_TEXTURE_2D);
 	glPushMatrix();
 	
-  //glBindTexture(GL_TEXTURE_2D, texture[0]);
-  //  Background cielo celeste
+	//glBindTexture(GL_TEXTURE_2D, texture[0]);
+	//  Background cielo celeste
   glBegin(GL_QUADS);
- 
 		glColor3f(0.35, 0.57, 0.984);
 		glVertex3f(Background[0].x, Background[0].y, Background[0].z);
 		glVertex3f(Background[1].x, Background[1].y, Background[1].z);
@@ -160,19 +162,15 @@ bool MyModel::DrawGLScene(void)
 
   for (float i = -1; i < 2.5; i += blockFloorLength){
 	  glBegin(GL_QUADS);
-
 			//basso sinistra
 		  glTexCoord2f(Background[0].u, Background[0].v);
 		  glVertex3f(i, Background[0].y, Background[0].z);
-
 		  //basso destra
 		  glTexCoord2f(Background[1].u, Background[1].v);
 		  glVertex3f(i + blockFloorLength, Background[1].y, Background[1].z);
-
 		  //alto destra
 		  glTexCoord2f(Background[2].u, Background[2].v);
 		  glVertex3f(i + blockFloorLength, Background[1].y + 0.3, Background[0].z);
-
 		  //alto sinistra
 		  glTexCoord2f(Background[3].u, Background[3].v);
 		  glVertex3f(i, Background[0].y + 0.3, Background[0].z);
@@ -209,14 +207,12 @@ bool MyModel::DrawGLScene(void)
   //Pacman
 
   //  Texture for the pacman, Full_elapsed * 19 -> change image every 1/19 sec.
-  //										% 18 -> pacman is contained between f[2] and f[11]
+  //										% 11 -> pacman is contained between f[2] and f[11]
 									//				(9 image + returning back)
 
-  // true cicle --> int texF = 2 + ((int((Full_elapsed * 19))) % 18);
-  // se sono 11 il modulo deve essere su 11
-  int texF = (int(Full_elapsed * 19) % 11) + 2  ;
-  if (texF > 11) {
-	  texF = 11;
+  int texF = (int(Full_elapsed * 19) % 19) + 2  ;
+  if (texF > 19) {
+	  texF = 2;
   }
 
   
@@ -226,7 +222,6 @@ bool MyModel::DrawGLScene(void)
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
   glEnable(GL_ALPHA_TEST);
   glAlphaFunc(GL_GREATER, 0);
-
   glBegin(GL_QUADS);
 
 	  //basso sinistra
@@ -244,9 +239,6 @@ bool MyModel::DrawGLScene(void)
 
   glEnd();
  
-
-
-
   /*
   //  Texture for the fire, change every 1/19 sec.
   int texF = 2 + ((int( (Full_elapsed*19) )) %26 );
@@ -284,7 +276,6 @@ bool MyModel::DrawGLScene(void)
 
   */
 
-
   //  Floating cursor - start
   /*
   glEnable(GL_BLEND);
@@ -297,66 +288,67 @@ bool MyModel::DrawGLScene(void)
   //  Disable texture to see the rectangle size
   //  The cursor "hot spot" is the center of the rectangle
   // glDisable(GL_TEXTURE_2D);
-  
-  glTranslatef(ClientX2World(cx), ClientY2World(cy), 0);
-  // proportional scaling (fixed percentual of window dimension)
-  // if(1) proportional, if(0) fixed
-   if( 1 ) glScalef(0.10f,0.10f,1);
-  //  Fixed scaling, alwais 100 pixels width/height
-   else {
-     float dx = PixToCoord_X(100);
-     float dy = PixToCoord_Y(100);
-     glScalef(dx/2, dy/2,1);
-   }
+ 
 
-   /*
-  glBegin(GL_QUADS);
-  for(int i = 0; i < 4; i++) {
+	glTranslatef(ClientX2World(cx), ClientY2World(cy), 0);
+	// proportional scaling (fixed percentual of window dimension)
+	// if(1) proportional, if(0) fixed
+	if (1) 
+	{
+		glScalef(0.10f, 0.10f, 1);
+	}
+	//  Fixed scaling, alwais 100 pixels width/height
+	else 
+	{
+		float dx = PixToCoord_X(100);
+		float dy = PixToCoord_Y(100);
+		glScalef(dx/2, dy/2,1);
+	}
+
+	/*
+	glBegin(GL_QUADS);
+	for(int i = 0; i < 4; i++) {
 		glTexCoord2f(curs[i].u, curs[i].v);
-    glVertex3f(curs[i].x, fire[i].y,  curs[i].z);
-  }
-  glEnd();
-  */
+	glVertex3f(curs[i].x, fire[i].y,  curs[i].z);
+	}
+	glEnd();
+	*/
 
-  glDisable(GL_BLEND);
-  glDisable(GL_ALPHA_TEST);
-  //  Floating cursor - end
+	glDisable(GL_BLEND);
+	glDisable(GL_ALPHA_TEST);
+	//  Floating cursor - end
 
-  //  Some text
-  glMatrixMode(GL_MODELVIEW);				// Select The Modelview Matrix
- 	glLoadIdentity();									// Reset The Current Modelview Matrix
+	//  Some text
+	glMatrixMode(GL_MODELVIEW);				// Select The Modelview Matrix
+	glLoadIdentity();									// Reset The Current Modelview Matrix
 	glDisable(GL_TEXTURE_2D);
 
- 	// Color
+	// Color
 	glColor3f(1.0f,1.0f,1.0f);
 
 	// Position The Text On The Screen
-  glRasterPos3f(- (float) plx + PixToCoord_X(10), (float) ply-PixToCoord_Y(21),
-    -4);
+	glRasterPos3f(- (float) plx + PixToCoord_X(10), (float) ply-PixToCoord_Y(21),-4);
 
-  // compute fps and write text
-  this->frames++;
-  if( this->frames > 18 ) {
-    this->fps = frames / frameTime;
-    this->frames = 0; this->frameTime = 0;
-  }
-  this->glPrint("Elapsed time: %6.2f sec.  -  Fps %6.2f",
-    Full_elapsed, fps);
+	// compute fps and write text
+	this->frames++;
+	if( this->frames > 18 ) {
+		this->fps = frames / frameTime;
+		this->frames = 0; this->frameTime = 0;
+	}
+	this->glPrint("Elapsed time: %6.2f sec.  -  Fps %6.2f", Full_elapsed, fps);
 
-  if(this->Full_elapsed < 6) {
-    glRasterPos3f(- (float) plx + PixToCoord_X(10), (float) -ply+PixToCoord_Y(21),
-    -4);
-    this->glPrint("...F2/F3/F4 for sounds");
-  }
+	if(this->Full_elapsed < 6) {
+		glRasterPos3f(- (float) plx + PixToCoord_X(10), (float) -ply+PixToCoord_Y(21),-4);
+		this->glPrint("...F2/F3/F4 for sounds");
+	}
 
-  {
-    glRasterPos3f(- (float) plx + PixToCoord_X(10), (float) -ply+PixToCoord_Y(61),
-    -4);
-    this->glPrint("%1d %1d  %s",cx,cy, captured ? "captured" : "Not captured" );
-  }
+	{
+		glRasterPos3f(- (float) plx + PixToCoord_X(10), (float) -ply+PixToCoord_Y(61),-4);
+		this->glPrint("%1d %1d  %s",cx,cy, captured ? "captured" : "Not captured" );
+	}
 
 	glEnable(GL_TEXTURE_2D);							// Enable Texture Mapping
-  return true;
+	return true;
 }
 
 
