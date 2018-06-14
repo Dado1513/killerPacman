d2acbfa69279e0d53ed33e365a91ad8e91ee5f8b
Dado1513 <dave.caputo93@gmail.com>
2018-06-14 18:56:32 +0200

    add clear stage

--- EnemySimple.cpp
+++ EnemySimple.cpp
@@ -2,8 +2,67 @@
 
 
 
-EnemySimple::EnemySimple()
+EnemySimple::EnemySimple(double posX, double posY, double width, double height){
+	this->posX = posX;
+	this->posY = posY;
+
+	this->width = width;
+	this->height = height;
+	this->velX = 0;
+	this->velMaxX = 0.00145;
+	this->accMaxX = 0.00002;
+
+}
+
+
+double EnemySimple::getLeft() {
+	return posX - width;
+}
+
+double EnemySimple::getVelMaxX() {
+	return this->velMaxX;
+}
+
+void EnemySimple::setVelMaxX(double velMaxX) {
+	this->velMaxX = velMaxX;
+}
+
+double EnemySimple::getVelX() {
+	return velX;
+}
+
+double EnemySimple::getRight() {
+	return posX + width;
+}
+
+double EnemySimple::getUp()
 {
+	return posY + height;
+}
+
+double EnemySimple::getDown()
+{
+	return posY - height;
+}
+
+void EnemySimple::setX(double posX)
+{
+	this->posX = posX;
+}
+
+void EnemySimple::setY(double posY)
+{
+	this->posY = posY;
+}
+
+double EnemySimple::getX()
+{
+	return this->posX;
+}
+
+double EnemySimple::getY()
+{
+	return this->posY;
 }
 
 
--- EnemySimple.h
+++ EnemySimple.h
@@ -7,6 +7,9 @@ class EnemySimple{
 		double width;
 		double height;
 		double velMaxX;
+		double velX;		
+		double accMaxX;
+
 	public:
 	EnemySimple(double posX, double posY, double width, double height);
 	~EnemySimple();
@@ -15,8 +18,15 @@ class EnemySimple{
 	double getRight();
 	double getUp();
 	double getDown();
+	double getVelX();
+
 	double getX();
 	double getY();
+	void setX(double posX);
+	void setY(double posY);
+	void setVelMaxX(double);
+	double getVelMaxX();
+
 
 };
 
